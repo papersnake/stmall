f1e9d364f29baa0c8b9a0ed30db1026c60b18420
papersnake <cctv5cn@gmail.com>
2013-10-15 16:59:37 +0800

    修正一些样式

--- Application/Admin/Controller/GoodController.class.php
+++ Application/Admin/Controller/GoodController.class.php
@@ -4,12 +4,18 @@ namespace Admin\Controller;
 
 class GoodController extends AdminController {
 
-	public function index($cate_id=null){
+	public function index($cate_id=null, $query=null){
 
 		$Good       = M('Good');
 		$map		= array();
 		if( $cate_id !== null ){
 			$map['category_id'] = array('like', $cate_id.'%');
+		}
+
+		if( isset($query) )
+		{
+			unset($map['category_id']);
+			$map['_string'] = "CONCAT(good_name,good_id,barcode,good_spec) like '%".$query."%'";
 		}
 		$list       = $this->lists($Good,$map,'id DESC');
 		//$menu       = D('GoodCategory')->getCategoryTree();
--- Application/Admin/Model/UserModel.class.php
+++ Application/Admin/Model/UserModel.class.php
@@ -2,7 +2,7 @@
 
 namespace Admin\Model;
 use Think\Model;
-use ORG\Util\Debug;
+//use ORG\Util\Debug;
 
 class UserModel extends Model {
 
