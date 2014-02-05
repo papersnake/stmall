7b88bc1d52e3ffc6ad7714abdd2d5745079fce2a
papersnake <cctv5cn@gmail.com>
2014-02-05 09:24:48 +0800

    fix sae uploads bug

--- Application/Admin/Conf/config_sae.php
+++ Application/Admin/Conf/config_sae.php
@@ -1,14 +1,15 @@
 <?php
 $st = 	new SaeStorage();
 return array(
+	'FILE_UPLOAD_TYPE'      =>  'Sae',    // 文件上传方式
 	'TMPL_PARSE_STRING'=>array(
-		'__STATIC__'  => __ROOT__ . '/Public/static',
-		'__ADDONS__'  => __ROOT__ . '/Public/' . MODULE_NAME . '/Addons',
-		'__IMG__'     => __ROOT__ . '/Public/' . MODULE_NAME . '/images',
-		'__CSS__'     => __ROOT__ . '/Public/' . MODULE_NAME . '/css',
-		'__JS__'      => __ROOT__ . '/Public/' . MODULE_NAME . '/js',
-		'__PLUGINS__' => __ROOT__ . '/Public/' . MODULE_NAME . '/plugins',
-		'__FONTS__'   => __ROOT__ . '/Public/' . MODULE_NAME . '/fonts',
+		'__STATIC__'       => __ROOT__ . '/Public/static',
+		'__ADDONS__'       => __ROOT__ . '/Public/' . MODULE_NAME . '/Addons',
+		'__IMG__'          => __ROOT__ . '/Public/' . MODULE_NAME . '/images',
+		'__CSS__'          => __ROOT__ . '/Public/' . MODULE_NAME . '/css',
+		'__JS__'           => __ROOT__ . '/Public/' . MODULE_NAME . '/js',
+		'__PLUGINS__'      => __ROOT__ . '/Public/' . MODULE_NAME . '/plugins',
+		'__FONTS__'        => __ROOT__ . '/Public/' . MODULE_NAME . '/fonts',
 		'/Uploads/Picture' =>	$st->getUrl('uploads','picture'),
 		)
 	);
