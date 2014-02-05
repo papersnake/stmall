2d457dae0d8d60463d80015745c1f1ae36433fed
papersnake <cctv5cn@gmail.com>
2014-02-05 08:51:53 +0800

    添加路径覆盖配置

--- Application/Admin/Conf/config_sae.php
+++ Application/Admin/Conf/config_sae.php
@@ -2,6 +2,13 @@
 $st = 	new SaeStorage();
 return array(
 	'TMPL_PARSE_STRING'=>array(
+		'__STATIC__'  => __ROOT__ . '/Public/static',
+		'__ADDONS__'  => __ROOT__ . '/Public/' . MODULE_NAME . '/Addons',
+		'__IMG__'     => __ROOT__ . '/Public/' . MODULE_NAME . '/images',
+		'__CSS__'     => __ROOT__ . '/Public/' . MODULE_NAME . '/css',
+		'__JS__'      => __ROOT__ . '/Public/' . MODULE_NAME . '/js',
+		'__PLUGINS__' => __ROOT__ . '/Public/' . MODULE_NAME . '/plugins',
+		'__FONTS__'   => __ROOT__ . '/Public/' . MODULE_NAME . '/fonts',
 		'/Uploads/Picture' =>	$st->getUrl('uploads','picture'),
 		)
 	);
