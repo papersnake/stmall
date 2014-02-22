0ea73b02fa3b9b8e775768e58a40e8bffe71e432
papersnake <cctv5cn@gmail.com>
2014-02-22 14:15:59 +0800

    fix a thinkphp bug add API/Good/Search

--- Application/Api/Conf/config.php
+++ Application/Api/Conf/config.php
@@ -2,6 +2,7 @@
 return array(
 	'URL_ROUTER_ON'   => true, 
 	'URL_ROUTE_RULES' => array(
+		'Good/search/:key' => array('Good/search','',array()),
 		'Good/:show' => array('Good/barcode','',array()),
 		),
 );
--- Application/Api/Controller/GoodController.class.php
+++ Application/Api/Controller/GoodController.class.php
@@ -12,16 +12,29 @@ Class GoodController extends RestController {
 		$this->response($this->goodsinfo,'JSON');
 	}
 
-	public function read_get_html(){
+	public function read_get_html() {
 		$id          = I('get.id','');
 		$this->goodsinfo = M('Good')->field(true)->find($id);
 		$this->response($this->goodsinfo,'JSON');
 	}
 
-	public function barcode_get_html(){
+	public function barcode_get_html() {
 		$barcode              = I('get.show','');
 		$condition['barcode'] = $barcode; 
 		$this->goodsinfo      = M('Good')->where($condition)->select();
+		$this->response($this->goodsinfo[0],'JSON');
+	}
+
+	public function search_get_html() {
+		$key             = I('get.key','');
+		$map['_string']  = "CONCAT(good_name,good_id,barcode,good_spec) like '%".$key."%'";
+		$this->goodsinfo = M('Good')->where($map)->select();
 		$this->response($this->goodsinfo,'JSON');
+		//header('application/json; charset=utf-8');
+		//echo json_encode($this->goodsinfo);
+		//$barcode              = I('get.keys','');
+		//$condition['barcode'] = $barcode; 
+		//$this->goodsinfo      = M('Good')->where($condition)->select();
+		//$this->response($this->goodsinfo,'JSON');
 	}
 }
