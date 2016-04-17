830b8f32b891cfea7fb31bee4e79343437765a1e
papersnake <cctv5cn@gmail.com>
2016-04-17 10:56:57 +0800

    更新openshift 用户密码

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -7,10 +7,13 @@ return array(
 	'DEFAULT_FILTER'  => 'htmlspecialchars',
 	/* 数据库配置 */
 	'DB_TYPE'         => 'mysql', // 数据库类型
-	'DB_HOST'         => '127.0.0.1', // 服务器地址
+	//'DB_HOST'         => '127.0.0.1', // 服务器地址
+	'DB_HOST'		=>'php',
 	'DB_NAME'         => 'stmall', // 数据库名
-	'DB_USER'         => 'root', // 用户名
-	'DB_PWD'          => '',  // 密码
+	//'DB_USER'         => 'root', // 用户名
+	'DB_USER'		=>'adminHN1c3in',
+	//'DB_PWD'          => '',  // 密码
+	'DB_PWD'		=>'DWR24b3hd91B',
 	'DB_PORT'         => '3306', // 端口
 	'DB_PREFIX'       => 'stmall_', // 数据库表前缀
 	
